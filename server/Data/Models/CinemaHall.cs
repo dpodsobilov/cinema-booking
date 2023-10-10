3a981c4906a6a100a838d4b97a68c5c256a4dd2d
Ruslan Ilin <111523409+Ruslanilin26@users.noreply.github.com>
2023-10-10 21:58:44 +0400

    add entity models (#2)
    * create entity models
    
    * fix models

---
+++ server/Data/Models/CinemaHall.cs
@@ -0,0 +1,17 @@
+namespace Data.Models;
+
+public class CinemaHall
+{
+    public int CinemaHallId { get; set; }
+    public string CinemaHallName { get; set; } = null!;
+    
+    // CinemaHallId -> FK в Session
+    public ICollection<Session> Sessions { get; set; } = null!;
+
+    //FK - CinemaHallTypeID
+    public int CinemaHallTypeId { get; set; }
+    public CinemaHallType CinemaHallType { get; set; } = null!;
+    //FK - CinemaId
+    public int CinemaId { get; set; }
+    public Cinema Cinema { get; set; } = null!;
+}
---
+++ server/Data/Models/User.cs
@@ -0,0 +1,12 @@
+namespace Data.Models;
+
+public class User {
+    public int UserId { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    
+    // UserID -> FK в Ticket
+    public ICollection<Ticket> Tickets { get; set; } = null!;
+}
