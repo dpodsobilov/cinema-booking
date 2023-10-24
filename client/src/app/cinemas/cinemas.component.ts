00849c434bedbe136f5d9de40f60919843018374
Ruslan <Ruslan.ilin.2002@yandex.ru>
2023-10-24 23:27:52 +0400

    add cinema selection

--- client/src/app/cinemas/cinemas.component.ts
+++ client/src/app/cinemas/cinemas.component.ts
@@ -4,20 +4,52 @@ export interface Film{
   name: string;
   // poster: string;
 }
+
+const film1: string = "Пираты карибского моря"
+const film2: string = "Железный человек"
+const film3: string = "Человек паук"
+const film4: string = "Зеленая миля"
+const film5: string = "Аватар"
+const film6: string = "Криминальное чтиво"
+
 @Component({
-  selector: 'app-cinemas',
-  templateUrl: './cinemas.component.html',
-  styleUrls: ['./cinemas.component.css']
+    selector: 'app-cinemas',
+    templateUrl: './cinemas.component.html',
+    styleUrls: ['./cinemas.component.css']
 })
 export class CinemasComponent {
+  films:string[] = []
+  cinemas:string[] = []
+  selectedCinema: string = ''
+  CINEMA = [
+      {
+          cinema: "Кинотеатр 1",
+          films: [film1, film2],
+      },
+      {
+          cinema: "Кинотеатр 2",
+          films: [film3, film4, film5, film6],
+      }
+  ]
+  constructor() {
+      this.films = [film1, film2, film3, film4, film5, film6]
+      for(let i = 0; i < this.CINEMA.length; i++ ){
+          this.cinemas[i] = this.CINEMA[i].cinema
+      }
+  }
 
-  film1 = "Пираты карибского моря"
-  film2= "Железный человек"
-  film3= "Человек паук"
-  film4= "Зеленая миля"
-  film5= "Аватар"
-  film6= "Криминальное чтиво"
-
-  films: string[] = [this.film1, this.film2, this.film3,this.film4,this.film5,this.film6]
-
+    onSelected(value: string){
+      this.selectedCinema = value
+      if(value === 'default'){
+          this.films = [film1, film2, film3, film4, film5, film6]
+      }
+        for(let i = 0; i < this.CINEMA.length; i++ ){
+            if(value === this.CINEMA[i].cinema){
+                this.films = this.CINEMA[i].films
+            }
+        }
+    }
 }
+
+
+
