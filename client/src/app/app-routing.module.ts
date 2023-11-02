aa96f588bdd7eb808e819f7de049023f250d9a43
denis <dpodsobilov29@gmail.com>
2023-11-02 10:44:01 +0400

    removing children in rounting

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: CinemasComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'tickets', component: UserTicketsComponent, children: [] },
+  { path: 'tickets', component: UserTicketsComponent },
   { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
 
