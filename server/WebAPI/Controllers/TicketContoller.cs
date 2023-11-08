bf0a54d1c5510422b6434bcb27e48ff682bc0408
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-11-08 23:30:23 +0400

    adding backend logic for tickets-history

--- server/WebAPI/Controllers/TicketContoller.cs
+++ server/WebAPI/Controllers/TicketContoller.cs
@@ -1,6 +1,25 @@
-﻿namespace WebAPI.Controllers;
+﻿using Logic.DTO;
+using Logic.Queries;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
 
-public class TicketContoller
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TicketController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public TicketController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
     
+    [HttpPost]
+    public async Task<AllUsersTicketsDto> GetSchedule([FromBody] int userId)
+    {
+        var tickets = await _mediator.Send(new GetUserTicketsQuery(userId));
+        return tickets;
+    }
 }
