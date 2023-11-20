0548439c9f11eb3f8b73488a4cab7495da878ea8
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-11-21 00:31:52 +0400

    implementing layout of admin-films and admin-genres

--- client/src/app/admin/films/films.component.ts
+++ client/src/app/admin/films/films.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AdminFilm,
+  AdminFilmService,
+} from '../../services/admin/admin-film.service';
 
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.css'],
 })
-export class FilmsComponent {}
+export class FilmsComponent implements OnInit {
+  films: AdminFilm[] = [
+    {
+      filmName: 'Три богатыря и Шамаханская царица',
+      filmId: 2,
+    },
+  ];
+
+  constructor(private adminFilmService: AdminFilmService) {}
+
+  ngOnInit(): void {
+    // this.adminFilmService.getFilms().subscribe((res: AdminFilm[]) => {
+    //   this.films = res;
+    // });
+  }
+}
--- client/src/app/admin/genres/genres.component.ts
+++ client/src/app/admin/genres/genres.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AdminFilmService,
+  AdminGenre,
+} from '../../services/admin/admin-film.service';
 
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.component.html',
   styleUrls: ['./genres.component.css'],
 })
-export class GenresComponent {}
+export class GenresComponent implements OnInit {
+  genres: AdminGenre[] = [
+    {
+      genreId: 1,
+      genreName: 'Жанр',
+    },
+  ];
+
+  constructor(private adminFilmService: AdminFilmService) {}
+
+  ngOnInit(): void {
+    // this.adminFilmService.getGenres().subscribe((res: AdminGenre[]) => {
+    //   this.genres = res;
+    // })
+  }
+}
