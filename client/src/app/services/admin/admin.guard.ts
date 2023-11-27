e178174312437790b7c2c9227a94dd306745c417
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-11-27 23:03:14 +0400

    refactor: changing let to const

--- client/src/app/services/admin/admin.guard.ts
+++ client/src/app/services/admin/admin.guard.ts
@@ -2,13 +2,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  let isAuth = !!localStorage.getItem('isAuthenticated')!;
+  const isAuth = !!localStorage.getItem('isAuthenticated')!;
   let isAdmin = false;
   if (isAuth) {
     isAdmin = !!JSON.parse(localStorage.getItem('user')!).info.role;
   }
   if (!isAuth || !isAdmin) {
-    let router = inject(Router);
+    const router = inject(Router);
     router.navigate(['/error']);
     return false;
   }
--- client/src/app/services/login.guard.ts
+++ client/src/app/services/login.guard.ts
@@ -2,10 +2,10 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const loginGuard: CanActivateFn = (route, state) => {
-  let isAuth = !!localStorage.getItem('isAuthenticated')!;
+  const isAuth = !!localStorage.getItem('isAuthenticated')!;
 
   if (isAuth) {
-    let router = inject(Router);
+    const router = inject(Router);
     router.navigate(['/error']);
     return false;
   }
