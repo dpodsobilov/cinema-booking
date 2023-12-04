4a15b8daa5c2d70d4333903dad4d85e13dc63fa6
droma0426 <droma0426@gmail.com>
2023-12-04 18:58:14 +0400

    feat: add client part of admin stats

---
+++ client/src/app/services/admin/admin-tickets.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AdminFilm, AdminGenre } from './admin-film.service';
+
+export interface FilmStatsString {
+  filmName: string;
+  orderedTickets: number;
+  totalTickets: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminTicketsService {
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_API_URL') private baseUrl: string,
+  ) {}
+
+  getStats(): Observable<FilmStatsString[]> {
+    return this.http.get<FilmStatsString[]>(this.baseUrl + '/Admin/Stats');
+  }
+}
