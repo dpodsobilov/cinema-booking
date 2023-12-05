08ce3dfc8ca51c6e3d1b8b48f6a0d5b7a42f3c8e
PerepechaevAlexander <120934668+PerepechaevAlexander@users.noreply.github.com>
2023-12-05 22:47:06 +0400

    Creation for Film, Genre (#28)

---
+++ server/Logic/DTO/Admin/ForCreating/CreationFilmDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Logic.DTO.Admin.ForCreating;
+
+public class CreationFilmDto
+{
+    public string FilmName { get; set; } = null!;
+    public string Hours { get; set; } = null!;
+    public string Minutes { get; set; } = null!;
+    public decimal FilmCoefficient { get; set; }
+    public string Description { get; set; } = null!;
+    public int Year { get; set; }
+    public string Poster { get; set; } = null!;
+    public int[] Genres { get; set; } = null!;
+}
