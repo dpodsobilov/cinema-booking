769106b48896091d284f90cfd8a4186a77c09f62
Ruslan <Ruslan.ilin.2002@yandex.ru>
2023-12-05 16:23:56 +0400

    add templates and delete for cinema, hall, template

--- client/src/app/admin/admin-cinemas/admin-cinemas.component.ts
+++ client/src/app/admin/admin-cinemas/admin-cinemas.component.ts
@@ -4,6 +4,7 @@ import {
   AdminCinemasService,
   AdminHalls,
 } from '../../services/admin/admin-cinemas.service';
+import { AdminSession } from '../../services/admin/admin-session.service';
 
 @Component({
   selector: 'app-admin-cinemas',
@@ -23,10 +24,26 @@ export class AdminCinemasComponent implements OnInit {
 
   selectStr(str: number) {
     this.selectedString = str;
+    this.adminCinemasService.selectedStr = str;
     this.adminCinemasService.hallsForSelectedCinema = [];
 
     this.adminCinemasService.getHalls(str).subscribe((res: AdminHalls[]) => {
       this.adminCinemasService.hallsForSelectedCinema = res;
+    });
+  }
+
+  cinemaDelete(cinemaId: number) {
+    this.adminCinemasService.deleteCinema(cinemaId).subscribe((response) => {
+      if (response.status === 200) {
+        this.adminCinemasService
+          .getCinemas()
+          .subscribe((res: AdminCinemas[]) => {
+            this.cinemas = res;
+          });
+        if (this.adminCinemasService.selectedStr === cinemaId) {
+          this.adminCinemasService.hallsForSelectedCinema = [];
+        }
+      } else alert('Ошибка! Удаление не выполнено!');
     });
   }
 }
--- client/src/app/admin/templates/templates.component.ts
+++ client/src/app/admin/templates/templates.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  AdminTemplates,
+  AdminTemplateService,
+} from '../../services/admin/admin-template.service';
 
 @Component({
   selector: 'app-templates',
   templateUrl: './templates.component.html',
   styleUrls: ['./templates.component.css'],
 })
-export class TemplatesComponent {}
+export class TemplatesComponent {
+  templates: AdminTemplates[] = [];
+  constructor(private adminTemplateService: AdminTemplateService) {}
+
+  ngOnInit() {
+    this.adminTemplateService
+      .getTemplates()
+      .subscribe((res: AdminTemplates[]) => {
+        this.templates = res;
+      });
+  }
+
+  deleteTemplate(templateId: number) {
+    this.adminTemplateService
+      .deleteTemplate(templateId)
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.adminTemplateService
+            .getTemplates()
+            .subscribe((res: AdminTemplates[]) => {
+              this.templates = res;
+            });
+        } else alert('Ошибка! Удаление не выполнено!');
+      });
+  }
+}
