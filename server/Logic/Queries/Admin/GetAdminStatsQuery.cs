c740fdf4c94660cd62e3f2d93517edb683f96663
denis <dpodsobilov29@gmail.com>
2023-12-05 13:16:34 +0400

    feat: changing sql-request for ordered tickets

--- server/Logic/Queries/Admin/GetAdminStatsQuery.cs
+++ server/Logic/Queries/Admin/GetAdminStatsQuery.cs
@@ -19,25 +19,25 @@ public class GetAdminStatsQueryHandler : IRequestHandler<GetAdminStatsQuery, ILi
     public async Task<IList<AdminStatDto>> Handle(GetAdminStatsQuery request, CancellationToken cancellationToken)
     {
 
-        //var sessions = await _applicationContext.Sessions
-        //    .Select(session => session)
-        //    .Distinct().ToListAsync(cancellationToken);
+        var orderedTicketsFilms = await _applicationContext.Films
+            .GroupJoin(
+                _applicationContext.Sessions,
+                film => film.FilmId,
+                session => session.FilmId,
+                (film, sessions) => new
+                {
+                    FilmName = film.FilmName,
+                    TicketCount = sessions
+                    .Join(
+                        _applicationContext.Tickets,
+                        session => session.SessionId,
+                        ticket => ticket.SessionId,
+                        (session, ticket) => ticket)
+                    .Count()
+                }
+            ).ToListAsync(cancellationToken);
 
-        //foreach(var session in sessions)
-        //{
-        //    var orderedTickets = await _applicationContext.Sessions.Count().;
-        //}
-
-        var result = await _applicationContext.Films
-            .GroupJoin(_applicationContext.Sessions, film => film.FilmId, session => session.FilmId, (film, sessions) => new { Film = film, Sessions = sessions })
-            .SelectMany(x => x.Sessions.DefaultIfEmpty(), (film, session) => new { Film = film.Film, Session = session })
-            .GroupJoin(_applicationContext.Tickets, x => x.Session.SessionId, ticket => ticket.SessionId, (x, tickets) => new { Film = x.Film, Tickets = tickets })
-            .SelectMany(x => x.Tickets.DefaultIfEmpty(), (x, ticket) => new { Film = x.Film, Ticket = ticket })
-            .GroupBy(x => x.Film.FilmName)
-            .Select(group => new { FilmName = group.Key, TicketCount = group.Count() })
-            .ToListAsync(cancellationToken);
-
-        var result2 = await _applicationContext.Films
+        var totalTicketsFilms = await _applicationContext.Films
                 .GroupJoin(
                     _applicationContext.Sessions,
                     film => film.FilmId,
@@ -65,56 +65,23 @@ public class GetAdminStatsQueryHandler : IRequestHandler<GetAdminStatsQuery, ILi
                     })
                 .ToListAsync(cancellationToken);
 
-        foreach (var item in result)
-        {
-            Console.WriteLine($"{item.FilmName}: {item.TicketCount} places");
-        }
+        totalTicketsFilms = totalTicketsFilms.OrderBy(f => f.FilmName).ToList();
 
-        Console.WriteLine("*************************************************");
+        orderedTicketsFilms = orderedTicketsFilms.OrderBy(f => f.FilmName).ToList();
 
-        foreach (var item in result2)
-        {
-            Console.WriteLine($"{item.FilmName}: {item.PlaceCount} places");
-        }
 
         var statDto = new List<AdminStatDto>();
 
-        for (int i  = 0; i < result.Count(); ++i)
+        for (int i  = 0; i < orderedTicketsFilms.Count(); ++i)
         {
             statDto.Add(new AdminStatDto
             {
-                FilmName = result[i].FilmName,
-                OrderedTickets = result[i].TicketCount,
-                TotalTickets = result2[i].PlaceCount
+                FilmName = orderedTicketsFilms[i].FilmName,
+                OrderedTickets = orderedTicketsFilms[i].TicketCount,
+                TotalTickets = totalTicketsFilms[i].PlaceCount
             });
         }
 
         return statDto;
-
-        //var tickets = await _applicationContext.Tickets
-        //   .GroupBy(ticket => ticket.Session.Film.FilmName)
-        //   .Select(group => new AdminStatDto
-        //   {
-
-        //   })
-        //   .ToListAsync(cancellationToken);
-        //var orderedTickets = await _applicationContext.Films
-        //    .Count(film => film.session)
-
-        //var stats = await _applicationContext.Films.Select(film => new AdminFilmDto
-        //{
-        //    FilmId = film.FilmId,
-        //    FilmName = film.FilmName
-        //}).ToListAsync(cancellationToken);
-
-        //await _applicationContext.Tickets
-        //    .Count(ticket => )
-        //    .Select(ticket => new AdminStatDto
-        //    {
-        //        FilmName = ticket.Session.Film.FilmName,
-        //        OrderedTickets = ticket.
-        //    }).ToListAsync(cancellationToken);
-
-        //return stats;
     }
 }
