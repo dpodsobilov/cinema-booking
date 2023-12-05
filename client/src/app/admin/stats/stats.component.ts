b543f8c074a782132d249069893070ff1969d0e1
Denis Podsobilov <112809750+dpodsobilov@users.noreply.github.com>
2023-12-05 19:13:36 +0400

    Creating admin statistics (#25)
    * feat: add client part of admin stats
    
    * feat: add server stats logic
    
    * feat: changing sql-request for ordered tickets
    
    * feat: adding percentage to stats to backend
    
    * feat: adding percentage to stats to frontend
    
    ---------
    
    Co-authored-by: droma0426 <droma0426@gmail.com>

--- client/src/app/admin/stats/stats.component.ts
+++ client/src/app/admin/stats/stats.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  AdminTicketsService,
+  FilmStatsString,
+} from '../../services/admin/admin-tickets.service';
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css'],
 })
-export class StatsComponent {}
+export class StatsComponent implements OnInit {
+  statistics: FilmStatsString[] = [];
+
+  constructor(private adminStatsService: AdminTicketsService) {}
+
+  ngOnInit(): void {
+    this.adminStatsService.getStats().subscribe((res: FilmStatsString[]) => {
+      this.statistics = res;
+    });
+  }
+}
