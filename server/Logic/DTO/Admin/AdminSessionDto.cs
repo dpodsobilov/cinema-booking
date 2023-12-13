6ffcfb71cc6efae39b538d1be47a178647e95b70
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-12-13 05:44:13 +0400

    feat: create session-modal

--- server/Logic/DTO/Admin/AdminSessionDto.cs
+++ server/Logic/DTO/Admin/AdminSessionDto.cs
@@ -3,9 +3,13 @@ namespace Logic.DTO.Admin;
 public class AdminSessionDto
 {
     public int SessionId { get; set; }
+    public int FilmId { get; set; }
     public string FilmName { get; set; } = null!;
     public string CinemaName { get; set; } = null!;
+    public decimal DataTimeCoefficient { get; set; }
+    public int CinemaHallId { get; set; }
     public string CinemaHallName { get; set; } = null!;
+    public DateTime DataTimeSession { get; set; }
     public string SessionDate { get; set; } = null!;
     public string SessionTime { get; set; } = null!;
 }
